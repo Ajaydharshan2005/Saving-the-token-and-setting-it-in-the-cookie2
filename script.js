a4ca436783f16030c0fe454c31511409ffe5a42a
Ajay Dharshan <ajaydharshan2005@gmail.com>
2025-04-07 11:07:10 +0530

    done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-const encrypt = (payload, secret) => {
-  // your code here and return token
-};
-
-module.exports = encrypt;
+const jwt = require('jsonwebtoken');
+ const encrypt = (payload, secret) => {
+   // your code here and return token
+   const token = jwt.sign(payload, secret, { expiresIn: '1m' }); // expires in 1 minute
+   return token;
+ };
+ 
+ module.exports = encrypt;
+ module.exports = encrypt;
